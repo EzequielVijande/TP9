d0e99889ed2e2bea4696489db86e062059951dbe
EzequielVijande <evijande@itba.edu.ar>
2017-06-12 19:05:32 -0300

    Primer commit

---
+++ TP9/snowflake.c
@@ -0,0 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+void create_koch_snowflake(int order, int length);
+//funcion que recibe el orden del fractal, en que coordenadas se lo desea crear.
+//dibuja un fractal de dicho orden en pantalla con un delay apreciable entre
+//cada iteracion.
+
