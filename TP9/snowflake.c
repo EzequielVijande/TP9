bde4b8544cd5c0dc7bd4656a8606bcfbdb7422a3
EzequielVijande <evijande@itba.edu.ar>
2017-06-12 21:01:40 -0300

    idea general

--- TP9/snowflake.c
+++ TP9/snowflake.c
@@ -3,8 +3,67 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-void create_koch_snowflake(int order, int length);
+
+#define CASO_BASE 0
+void create_koch_snowflake(int order, double length);
 //funcion que recibe el orden del fractal, en que coordenadas se lo desea crear.
 //dibuja un fractal de dicho orden en pantalla con un delay apreciable entre
 //cada iteracion.
 
+void create_koch_snowflake(int n, double length)
+{
+    int i;
+    for(i=0; i<3; i++)
+    {
+        if(n==CASO_BASE)
+        {
+            draw_line(length/3); 
+        }
+        else
+        {
+            create_koch_snowflake(n-1, length/3.0);
+        }
+
+        rotate_anticlockwise(60);
+
+        if(n==CASO_BASE)
+        {
+            draw_line(length/3);
+        }
+        else
+        {
+            create_koch_snowflake(n-1, length/3.0);
+        }
+
+        rotate_clockwise(120);
+
+        if(n==CASO_BASE)
+        {
+            draw_line(length/3);
+        }
+        else
+        {
+            create_koch_snowflake(n-1, length/3.0);
+        }
+
+        rotate_anticlockwise(60);
+
+        if(n==CASO_BASE)
+        {
+            draw_line(length/3);
+        }
+        else
+        {
+            create_koch_snowflake(n-1, length/3.0);
+        }
+
+        rotate_clockwise(120);
+    }
+    
+    
+    
+    
+    
+    
+    
+}
